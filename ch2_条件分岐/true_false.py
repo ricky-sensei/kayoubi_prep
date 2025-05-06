18884c1f78282718fb7d0b084a2aa14588fc2729
ricky-sensei <axcelcamp.teacher@gmail.com>
2025-05-06 22:58:59 +0900

    やっぱりかっこいい

---
+++ ch2_条件分岐/true_false.py
@@ -0,0 +1,18 @@
+# weather = "晴れ"
+
+# if weather == "晴れ": 
+#     print("運動会を行います")
+
+# else:
+#     print("運動会は明日です")
+
+# weather = "晴れ"
+
+# print(weather == "晴れ")
+# print(weather == "雨")
+
+
+ricky_face_hensachi = 200
+
+print(isinstance(ricky_face_hensachi, int))
+print(isinstance(ricky_face_hensachi, str))
