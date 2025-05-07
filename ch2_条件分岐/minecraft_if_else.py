1107360e671fe7985f51b2f20e3150431e534819
ricky-sensei <axcelcamp.teacher@gmail.com>
2025-05-08 00:15:17 +0900

    5-8

---
+++ ch2_条件分岐/minecraft_if_else.py
@@ -0,0 +1,14 @@
+mode = "survival"
+
+if mode == "survival":
+    print("ダメージを食らう")
+
+elif mode == "creative":
+    print("ダメージをくらわない")
+    print("アイテムをなんでも持ってる")
+
+elif mode == "adventure":
+    print("ブロックをこわせない")
+
+else:
+    print("")
